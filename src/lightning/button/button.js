3c3d353b7015ee94b731206f844f739a153a2a88
reiniergs <reiniergs@gmail.com>
2019-08-31 17:45:46 -0400

    feat: implement button component

---
+++ src/lightning/button/button.js
@@ -0,0 +1,31 @@
+import { LightningElement, api } from 'lwc';
+import classnames from 'classnames'
+
+export default class LightningBadge extends LightningElement {
+    @api label;
+
+    @api disabled;
+
+    @api type = 'button';
+
+    @api variant;
+
+    get computedClass() {
+        return classnames('slds-button', {
+            'slds-button_neutral': this.variant === 'neutral',
+            'slds-button_brand': this.variant === 'brand',
+            'slds-button_outline-brand': this.variant === 'outline-brand',
+            'slds-button_inverse': this.variant === 'inverse',
+            'slds-button_destructive': this.variant === 'destructive',
+            'slds-button_success': this.variant === 'success',
+        });
+    }
+
+    /**
+     * Sets focus on the button.
+     */
+    @api
+    focus() {
+        this.template.querySelector('button').focus();
+    }
+}
