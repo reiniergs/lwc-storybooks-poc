adcfdcd877f951224ab419645159eab8e0ae4134
reiniergs <reiniergs@gmail.com>
2019-09-01 04:52:35 -0400

    feat: add addon-knobs

--- stories/badge.stories.js
+++ stories/badge.stories.js
@@ -1,23 +1,25 @@
 import { document, console } from 'global';
 import { storiesOf } from '@storybook/html';
 import { createElement } from '@lwc/engine';
+import { withKnobs, text } from '@storybook/addon-knobs';
 import Badge from 'lightning/badge';
 
 storiesOf('Badge', module)
+    .addDecorator(withKnobs)
     .add('default', () => {
         const badge = createElement('lightning-badge', { is: Badge });
-        badge.label = 'Default Badge';
+        badge.label = text('label', 'Default Badge');
         return badge;
     })
     .add('inverse', () => {
         const badge = createElement('lightning-badge', { is: Badge });
-        badge.label = 'Inverse Badge';
+        badge.label = text('label', 'Inverse Badge');
         badge.variant = 'inverse';
         return badge;
     })
     .add('lightest', () => {
         const badge = createElement('lightning-badge', { is: Badge });
-        badge.label = 'Lightest Badge';
+        badge.label = text('label', 'Lightest Badge');
         badge.variant = 'lightest';
         return badge;
     });
