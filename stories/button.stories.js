9d5d2d3a18a2f50bbbd473c08d53bd22fdeaf259
Reinier Guerra <reinier.guerra@salesforce.com>
2019-09-12 12:35:04 -0400

    chore: clean up

--- stories/button.stories.js
+++ stories/button.stories.js
@@ -1,45 +1,33 @@
 import { storiesOf } from '@storybook/html';
 import { buildCustomElementConstructor, createElement } from '@lwc/engine';
-import { withKnobs, boolean, text } from '@storybook/addon-knobs';
 import Button from 'lightning/button';
 
 const buttonCustomElement = buildCustomElementConstructor(Button);
 customElements.define('lightning-button', buttonCustomElement); 
 
 storiesOf('Button', module)
-    .addDecorator(withKnobs)
     .add('base', () => `
-        <lightning-button label="Base Button"><lightning-button>
+        <div class="slds-p-around_medium">
+            <lightning-button label="Base Button"><lightning-button>
+        </div>    
     `)
-    .add('neutral', () => {
-        const button = createElement('lightning-button', { is: Button });
-        button.label = text('label', 'Neutral Button');
-        button.disabled = boolean('disabled', false);
-        button.variant = 'neutral';
-        button.addEventListener('click', () => alert('neutral button clicked'));
-        return button;
-    })
-    .add('brand', () => {
-        const button = createElement('lightning-button', { is: Button });
-        button.label = text('label', 'Brand Button');
-        button.disabled = boolean('disabled', false);
-        button.variant = 'brand';
-        button.addEventListener('click', () => alert('brand button clicked'));
-        return button;
-    })
-    .add('destructive', () => {
-        const button = createElement('lightning-button', { is: Button });
-        button.label = text('label', 'Destructive Button');
-        button.disabled = boolean('disabled', false);
-        button.variant = 'destructive';
-        button.addEventListener('click', () => alert('destructive button clicked'));
-        return button;
-    })
-    .add('success', () => {
-        const button = createElement('lightning-button', { is: Button });
-        button.label = text('label', 'Success Button');
-        button.disabled = boolean('disabled', false);
-        button.variant = 'success';
-        button.addEventListener('click', () => alert('success button clicked'));
-        return button;
-    });
+    .add('neutral', () => `
+        <div class="slds-p-around_medium">
+            <lightning-button variant="neutral" label="Neutral Button"><lightning-button>
+        </div>    
+    `)
+    .add('brand', () => `
+        <div class="slds-p-around_medium">
+            <lightning-button variant="brand" label="Brand Button"><lightning-button>
+        </div>    
+    `)
+    .add('destructive', () => `
+        <div class="slds-p-around_medium">
+            <lightning-button variant="destructive" label="Destructive Button"><lightning-button>
+        </div>    
+    `)
+    .add('success', () => `
+        <div class="slds-p-around_medium">
+            <lightning-button variant="success" label="Success Button"><lightning-button>
+        </div>    
+    `);
--- stories/card.stories.js
+++ stories/card.stories.js
@@ -7,13 +7,15 @@ customElements.define('lightning-card', cardCustomElement);
 
 storiesOf('Card', module)
     .add('basic', () => `
-        <lightning-card title="Accounts">
-            <lightning-button
-                slot="actions"
-                label="New"
-                variant="neutral">
-            </lightning-button>
-            <p style="padding: 0 1rem">Anything can go into the card body</p>
-            <span slot="footer">View All</span>
-        </lightning-card>
+        <div class="slds-p-around_medium">
+            <lightning-card title="Accounts">
+                <lightning-button
+                    slot="actions"
+                    label="New"
+                    variant="neutral">
+                </lightning-button>
+                <p style="padding: 0 1rem">Anything can go into the card body</p>
+                <span slot="footer">View All</span>
+            </lightning-card>
+        <div class="slds-p-around_medium">    
     `);
