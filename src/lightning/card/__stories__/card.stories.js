73465d042848ca878b76bd191a11c5d0639f597c
Matt Rigdon <mrigdon@salesforce.com>
2019-10-01 10:59:21 -0700

    different structure

---
+++ src/lightning/card/__stories__/card.stories.js
@@ -0,0 +1,27 @@
+import { document } from 'global';
+import { buildCustomElementConstructor } from '@lwc/engine';
+import Card from 'lightning/card';
+
+const cardCustomElement = buildCustomElementConstructor(Card);
+customElements.define('lightning-card', cardCustomElement);
+
+export default {
+    title: 'Card'
+};
+
+export const basic = () => {
+    const div = document.createElement('div');
+    div.className = 'slds-p-around_medium';
+    div.innerHTML = `
+      <lightning-card title="Accounts">
+          <lightning-button
+              slot="actions"
+              label="New"
+              variant="neutral">
+          </lightning-button>
+          <p style="padding: 0 1rem">Anything can go into the card body</p>
+          <span slot="footer">View All</span>
+      </lightning-card>
+    `;
+    return div;
+};
