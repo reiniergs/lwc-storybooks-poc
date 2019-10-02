dda9c38d067322c43547339245c3ec15d844092d
Matt Rigdon <mrigdon@salesforce.com>
2019-10-02 09:57:43 -0700

    better card story

--- src/lightning/card/__stories__/card.stories.js
+++ src/lightning/card/__stories__/card.stories.js
@@ -8,15 +8,11 @@ export const basic = () => {
   const div = document.createElement('div');
   div.className = 'slds-p-around_medium';
   div.innerHTML = `
-      <lightning-card title="Accounts">
-          <lightning-button
-              slot="actions"
-              label="New"
-              variant="neutral">
-          </lightning-button>
-          <p style="padding: 0 1rem">Anything can go into the card body</p>
-          <span slot="footer">View All</span>
-      </lightning-card>
-    `;
+    <lightning-card title="Accounts">
+      <button slot="actions">New</button>
+      <p style="padding: 0 1rem">Anything can go into the card body</p>
+      <span slot="footer">View All</span>
+    </lightning-card>
+  `;
   return div;
 };
